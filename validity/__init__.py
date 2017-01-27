b5ebc7271b88f52c80f16e5cae5aa495d963c46e
x <Ya.Ivanukh@gmail.com>
2017-01-27 17:33:58 +0200

    bugfix (missed ",")

--- validity/__init__.py
+++ validity/__init__.py
@@ -2,5 +2,5 @@ from .comparator import BaseComparator, GT, GTE, LT, LTE, EQ, NotEQ, Any, Betwee
 from .logical_operator import Base, BaseLogicalOperator, Or, And, Not
 # TODO: ConvertTo(type, conditions)
 
-__all__ = ['BaseComparator', 'GT', 'GTE', 'LT', 'LTE', 'EQ', 'NotEQ', 'Any', 'Between', 'TypeIs', 'Len', 'Count'
+__all__ = ['BaseComparator', 'GT', 'GTE', 'LT', 'LTE', 'EQ', 'NotEQ', 'Any', 'Between', 'TypeIs', 'Len', 'Count',
            'Base', 'BaseLogicalOperator', 'Or', 'And', 'Not']
